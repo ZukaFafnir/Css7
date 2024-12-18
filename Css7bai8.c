c44d908f760f3f8464b0d3c18bc3286b9f263caa
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-19 02:52:52 +0700

    Add files via upload

---
+++ Css7bai8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+	int row,column;
+	printf("Please enter the number of rows and columns of a two-dimensional array:  ");
+	scanf("%d%d",&row,&column);
+	int array[row][column];
+    printf("Please enter the elements:\n");
+	for(int i=0;i<sizeof(array)/sizeof(array[0]);i++){
+		for(int j=0;j<column;j++){
+			printf("[%d][%d] of array is: ",i,j);
+			scanf("%d",&array[i][j]);
+			
+		}
+	}
+	printf("The values of the elements are:\n");
+	for(int i=0;i<row;i++){
+		for(int j=0;j<column;j++){
+			printf(" %d",array[i][j]);
+		}
+		printf("\n");
+
+	}
+	return 0;
+}
