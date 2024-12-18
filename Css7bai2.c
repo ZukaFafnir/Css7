3c16889cc2ec3b82197672fd3bfbeacdf34054ed
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-18 21:37:49 +0700

    Add files via upload

--- Css7bai2.c
+++ Css7bai2.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 int main()
-{ int n[5]={3,5,3};
-for(int i = 0;i<=sizeof(n)/sizeof(int);i++){
+{ int n[5];
 
+	
+		for(int i = 1;i<=sizeof(n)/sizeof(int);i++){
 	printf(" Moi ban nhap phan tu thu %d ",i);
-	scanf("%d",&n[i]);
-	printf(" Phan tu thu %d la %d\n",i,n[i]);
-	}	
+	scanf("%d",&n[i]);}
+	printf("Nhung mang vua nhap lan luot la\n");
+	for(int i=1;i<=sizeof(n)/sizeof(int);i++){
+		
+		printf("%d  ",n[i]);
+	}
+
+	
+		
+		
+		
+
+	
 	return 0;
 }
