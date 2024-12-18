ac9742d91e9206345b9b30f263524e12cfda57c0
Nguyen Van Minh Tri <mtanime020705@gmail.com>
2024-12-18 20:10:51 +0700

    Add files via upload

---
+++ Css7bai4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("moi ban nhap kich thuoc mang ");
+	scanf("%d",&n); 
+	int mang[n];
+	for(int i=1;i<=n;i++){
+	
+	printf(" moi ban nhap nhung phan tu ");
+	scanf("%d",&mang[i]);
+}
+return 0;	
+}
